16e2200fdac1b44f4a3d703a3d9175c3c02e19c5
RyanGelow <exisphere@gmail.com>
2020-01-27 13:40:12 -0800

    revised footer

--- src/Partials/Footer/index.js
+++ src/Partials/Footer/index.js
@@ -2,13 +2,11 @@ import React from "react";
 
 const Footer = props => (
     <div className="page-footer bg-dark pt-4 border-top border-secondary">
-        <div className="container-fluid text-center text-md-left">
-            <div className="row">
-                <div className="col-md-6 mt-md-0 mt-3 mb-3">
-                    <h5 className="text-light">Disco Mosh Pit</h5>
-                    <p><small className="text-light">Thanks for visiting!</small></p>
-                    <small className="text-light">This is a <a className="text-info" href="http://www.ryangelow.com">RyanGelow</a> production.</small>
-                </div>
+        <div className="container-fluid text-md-left">
+            <div className="pt-3 pb-3">
+                <h5 className="text-light text-center">Disco Mosh Pit</h5>
+                <p className="text-light text-center"><small>Thanks for visiting!</small></p>
+                <p className="text-light text-center"><small>This is a <a className="text-info" href="http://www.ryangelow.com">RyanGelow</a> production.</small></p>
             </div>
         </div>
     </div>
